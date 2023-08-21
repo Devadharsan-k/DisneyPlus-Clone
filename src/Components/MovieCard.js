e9755506d0a6aad88b4776febe629365b856aabb
Devadharsan-k <111576272+Devadharsan-k@users.noreply.github.com>
2023-08-21 07:23:22 +0530

    Update MovieCard.js

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -45,7 +45,7 @@ const MovieCard = ({ movie }) => {
 
 
 const Wrap = styled.div`
-  padding-top: 56.25%;
+  padding-top: 150%;
   border-radius: 10px;
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
